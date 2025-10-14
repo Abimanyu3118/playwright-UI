3195af19cd9c4a6586c9452ddb21f0e8172edc33
Abimanyu Mani <m.abimanyu@gmail.com>
2025-10-15 10:46:54 +1100

    ci-pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
